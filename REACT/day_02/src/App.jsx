da9b6e42a4cdcb6721475f46118045052f9a0ada
Param895 <paramjot895@gmail.com>
2026-02-06 14:37:55 +0530

    this is

---
+++ REACT/day_02/src/App.jsx
@@ -0,0 +1,13 @@
+import Component from "./Component.jsx";
+
+function App(){
+  return (
+    <>
+    <h1>First function</h1>
+    <p>This is the main app function </p>
+    <Component/>
+    </>
+  )
+}
+
+export default App;
---
+++ REACT/day_02/src/Component.jsx
@@ -0,0 +1,10 @@
+function Component(){
+        return (
+        <>
+         <h1>This the text from Compoment</h1>
+        <p> Here is the text is component </p>
+        </>
+    );
+}
+
+export default Component;
