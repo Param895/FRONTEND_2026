75bfd54d4d771928db2ae38d6be401423e276e7d
Param895 <paramjot895@gmail.com>
2026-02-11 01:11:41 +0530

    learnt how to implement css in react

--- REACT/day_05/src/Form.jsx
+++ REACT/day_05/src/Form.jsx
@@ -1,16 +1,20 @@
 import {useState} from 'react';
+import './index.css'
 
 function Form (){
     const [fName, setfName] = useState("");
     return(
-        <>
-        <h1>Form For collecting information </h1>
+
+        <div> 
+            <h1>Form For collecting information </h1>
         <form action="">
             <input type="text" onChange={(e)=>setfName(e.target.value) }/>
         </form>
         
         <h2>{fName}</h2>
-        </>
+        </div>
+        
+    
     )
 }
 
